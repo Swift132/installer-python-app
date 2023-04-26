396936427f1b648fe0760fd19c6f109b87dddd95
Diogo Pacheco <102195051+Swift132@users.noreply.github.com>
2023-04-26 17:35:14 +0000

    Add files via upload

---
+++ installer-scripts-main/README.md
@@ -0,0 +1,114 @@
+# Installer Scripts
+
+This repository contains two Bash scripts designed to automate software and extension installation processes for developers.
+
+## Author
+
+2023 - By João Vieira | `jvieira9` on [GitHub](https://github.com/jvieira9)
+
+### Script 1 - software.sh
+
+This script has the possibility of installing the following software:
+
+- Google Chrome
+- Visual Studio Code
+- Git
+- Docker
+- Python
+- Node.js
+- VirtualBox
+- VLC
+- Notepadqq
+- Discord
+- KeePass2
+- Spotify
+- GIMP
+
+### Functionality
+
+1. The script creates two log files: `install.log` to register all the commands executed by the script and `error.log` to register any errors that occur while executing the script.
+2. The script checks for an internet connection and exits if there is none.
+3. The script checks if the user running the script has sudo privileges and exits if not.
+4. The script updates the package index, upgrades system packages, and refreshes snap packages.
+5. The script prompts the user to select which software to install. If the user selects a software that is already installed, the script notifies them.
+6. The script prompts the user to choose whether to delete the log files created by the script.
+
+### Script 2 - extensions.sh
+
+This script has the possibility of installing the following extensions:
+
+- Python
+- Shellman
+- Docker
+- Intellicode
+- Node.js extension pack 
+
+### Functionality
+
+1. The script creates two log files: `extensions-install.log` to register all the commands executed by the script and `extensions-error.log` to register any errors that occur while executing the script.
+2. The script checks for an internet connection and exits if there is none.
+3. The script checks if Visual Studio Code is installed on the device and exits if not.
+4. The script prompts the user to select which extensions to install. If the user selects an extension that is already installed, the script notifies them.
+5. The script prompts the user to choose whether to delete the log files created by the script.
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+## Getting Started
+
+To execute the scripts on a new machine, follow these steps:
+
+1. Clone this repository to your local machine using the command:
+
+    ```
+    git clone https://github.com/jvieira9/installer-scripts.git
+    ```
+
+2. Navigate to the directory where the repository was cloned using the command:
+
+    ```
+    cd installer-scripts
+    ```
+
+3. Modify the permissions of the scripts before executing them. To do this, run the following command: 
+
+    ```
+    chmod +x software.sh extensions.sh
+    ```
+
+4. Run the following command to execute the first script: 
+
+    ```
+    ./software.sh
+    ```
+
+5. Run the following command to execute the second script: 
+
+    ```
+    ./extensions.sh
+    ```
+
+## Notes
+
+- These scripts were designed to run on Ubuntu 22.04 but can run on most Debian-based Linux distributions, such as Linux Mint, Ubuntu, Debian, Xubuntu, and Lubuntu.
+- To execute these script, you need to have an internet connection.
+- To execute the `software.sh`, you need to have sudo privileges.
+- To execute the `extensions.sh` script, you need Visual Studio Code installed on your computer.
+
+# Table of ANSI for colors
+
+| ANSI Escape Code | Description |
+| ------------- | ------------- |
+| \033[0m       | Resetar       |
+| \033[1m       | Negrito       |
+| \033[2m	    | Faint         |
+| \033[3m	    | Itálico       |
+| \033[4m	    | Sublinhado    |
+| \033[5m	    | Negrito       |
+| \033[7m	    | Invertido     |
+| \033[8m	    | Escondido     |
+| \033[9m	    | Riscado       |
+| \033[1m	    | Negrito       |
+
+#### If you encounter any issues while executing these scripts, please feel free to contact me at `joao.lopes.vieira115@gmail.com`.
+
+
---
+++ installer-scripts-main/extensions.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+# 2023 - By: João Vieira | 'jvieira9' on GitHub
+# This script automates the installation of Visual Studio Code extensions on an Ubuntu-based system.
+# VS Code Extensions: Python, Shellman, Docker, Intellicode, Node.js extension pack.
+
+# Remove log files from previous executions of the script
+rm extensions-install.log >/dev/null 2>&1
+rm extensions-error.log >/dev/null 2>&1
+
+# Create a new extensions-install.log file and log the first message
+printf "This file registers every command executed by this script.\n" >> extensions-install.log
+
+# This enables the script to exit immediately if any command fails
+set -euo pipefail
+
+# Internet connection check
+if ! ping -c 2 google.com >> extensions-install.log 2> extensions-error.log && ! ping -c 2 microsoft.com >> extensions-install.log 2> extensions-error.log; then
+    printf "No internet connection. The script cannot be executed.\n"
+    rm extensions-install.log >/dev/null 2>&1
+    rm extensions-error.log >/dev/null 2>&1
+    exit 1
+fi
+
+# VS Code check
+if ! command -v code &> /dev/null; then
+    printf "Visual Studio Code is not installed. Aborting.\n"
+    rm extensions-install.log >/dev/null 2>&1
+    rm extensions-error.log >/dev/null 2>&1
+    exit 1
+fi
+
+# Print the list of software that can be installed
+printf "Type the digit that corresponds to the extension you wish to install and press 'Enter', afterwards select the option 'Finished' to proceed with the installation.\n" 
+printf "Select 'None' to exit without installing any software.\n"
+printf " \n"
+options=("None" "Python" "Shellman" "Docker" "IntelliCode" "Node.js extension pack" "Finished")
+selected=()
+
+# Loop until the user is done selecting options
+while true; do
+  select opt in "${options[@]}"; do
+    case $opt in
+      "Finished")
+        # Check if the user has selected any packages. If not, prompt the user to select at least one package.
+        if [ ${#selected[@]} -eq 0 ]; then
+          printf "Please select at least one option or 'None' to exit.\n"
+        else
+          break 2 # Exit both loops and proceed to install the selected packages.
+        fi
+        ;;
+      "None")
+        printf " \n"
+        printf "Exiting without installing any software.\n"
+        rm extensions-install.log >/dev/null 2>&1
+        rm extensions-error.log >/dev/null 2>&1
+        exit 0
+        ;;
+      *)
+        # Check if the user has already selected the package. If not, add it to the list of selected packages.
+        if [[ " ${selected[@]} " =~ " ${opt} " ]]; then
+          printf "%s is already selected.\n" "$opt"
+        else
+          selected+=("$opt")
+          printf "%s selected.\n" "$opt"
+        fi
+        ;;
+    esac
+  done
+done
+
+# Install the selected extensions
+for opt in "${selected[@]}"; do
+  case $opt in
+
+    "Python")
+        # Check if Python Extension is already installed. If not, install it.
+        if ! code --list-extensions | grep -q "ms-python.python"; then 
+            printf "Installing the Python extension...\n"
+            code --install-extension ms-python.python >> extensions-install.log 2> extensions-error.log
+            printf "The Python extension was succesfully installed.\n"
+        else
+            printf "The Python extension is already installed.\n"
+        fi
+    ;;
+
+    "Docker")
+        # Check if Docker Extension is already installed. If not, install it.
+        if ! code --list-extensions | grep -q "ms-azuretools.vscode-docker"; then
+            printf "Installing the Docker extension...\n"
+            code --install-extension ms-azuretools.vscode-docker >> extensions-install.log 2> extensions-error.log
+            printf "The Docker extension was succesfully installed.\n"
+        else
+            printf "The Docker extension is already installed.\n"
+        fi
+    ;;
+
+    "IntelliCode")
+        # Check if IntelliCode Extension is already installed. If not, install it.
+        if ! code --list-extensions | grep -q "VisualStudioExptTeam.vscodeintellicode"; then
+            printf "Installing the IntelliCode extension...\n"
+            code --install-extension VisualStudioExptTeam.vscodeintellicode >> extensions-install.log 2> extensions-error.log
+            printf "The IntelliCode extension was succesfully installed.\n"
+        else
+            printf "The IntelliCode extension is already installed.\n"
+        fi
+    ;;
+
+    "Shellman")
+        # Check if shellman Extension is already installed. If not, install it.
+        if ! code --list-extensions | grep -q "Remisa.shellman"; then
+            printf "Installing the shellman extension...\n"
+            code --install-extension Remisa.shellman >> extensions-install.log 2> extensions-error.log
+            printf "The Shellman extension was succesfully installed.\n"
+        else
+            printf "The Shellman extension is already installed.\n"
+        fi
+    ;;
+
+    "Node.js extension pack")
+        # Check if Node.js extension pack is already installed. If not, install it.
+        if ! code --list-extensions | grep -q "waderyan.nodejs-extension-pack"; then
+            printf "Installing the Node.js extension pack...\n"
+            code --install-extension waderyan.nodejs-extension-pack >> extensions-install.log 2> extensions-error.log
+            printf "The Node.js extension pack was succesfully installed.\n"
+        else
+            printf "The Node.js extension pack is already installed.\n"
+        fi
+    ;;
+  esac
+done
+
+# Log files
+printf " \n"
+read -p "Would you like to delete the log files created by this script? [Y/n]" log
+if [[ "$log" == [yY] || "$log" == "" ]]; then
+        rm extensions-install.log >/dev/null 2>&1
+        rm extensions-error.log >/dev/null 2>&1
+fi
+
+# Installation Complete
+printf " \n"
+printf "Installation complete!\n"
